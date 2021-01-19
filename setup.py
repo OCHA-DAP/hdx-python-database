b5bc76fea4c7336f0c2d5ea52358a92ee19c5b95
Mike <rans@un.org>
2021-01-20 10:28:36 +1300

    Make postgres psycopg2 functionality optional

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     zip_safe=True,
     classifiers=classifiers,
     install_requires=requirements,
-    extras_requires=extras_requirements,
+    extras_require=extras_requirements,
     cmdclass={'clean': CleanCommand, 'package': PackageCommand, 'publish': PublishCommand},
 )
