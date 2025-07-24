81ea25b03a8bcf6cc0de8a4a58741377bdebe171
mcarans <rans@email.com>
2025-07-24 13:32:07 +1200

    Fix styling

--- tests/hdx/database/test_drop_recreate.py
+++ tests/hdx/database/test_drop_recreate.py
@@ -7,6 +7,7 @@ def test_drop_schema(mock_engine):
     db_uri = "Error"
     assert Database.drop_schema(mock_engine, db_uri) is False
 
+
 def test_recreate_schema(mock_engine):
     db_uri = "postgresql+psycopg://myuser:mypass@0.0.0.0:12345/mydatabase"
     assert Database.recreate_schema(mock_engine, db_uri) is True
